bfb345110fc0a04f3d7c47eee78aa45db8021305
Pierre Boutet <boutetpierre@yahoo.fr>
2019-01-17 10:21:07 +0100

    change time for css changes

--- modules/page-loading-spinner.js
+++ modules/page-loading-spinner.js
@@ -3,17 +3,20 @@ $('.loadingLayer').appendTo('body');
 
 $('document').ready(function () {
 
-	$('body').css('position', 'relative');
-	$('body').css('right', 'initial');
-	$('body').css('left', 'initial');
 
 	var action = mw.config.get('wgAction');
 
 	if (action == 'formedit') {
 		mw.hook( 'pf.formInitializationEnd' ).add( function(div) {
+			$('body').css('position', 'relative');
+			$('body').css('right', 'initial');
+			$('body').css('left', 'initial');
 			$( ".loadingLayer" ).hide();
 		});
 	} else {
+		$('body').css('position', 'relative');
+		$('body').css('right', 'initial');
+		$('body').css('left', 'initial');
 		$( ".loadingLayer" ).hide();
 	}
 
